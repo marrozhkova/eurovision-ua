18bda684dc89a99ded4f9e19e757502071ffeeda
marrozhkova <mar.rozhkova@gmail.com>
2025-02-26 18:17:41 +0100

    heart-v.1

--- src/components/Verka.jsx
+++ src/components/Verka.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import heartRain from "../js//heartRain";
 
 const Verka = () => {
   const [showVideo, setShowVideo] = useState(false);
+  useEffect(() => {
+    heartRain();
+  }, []);
 
   return (
     <div className="verka page-transition">
       <h1>Verka Serduchka 2007</h1>
+      <div className="absolut">
+        <div className="heart-container">
+          <div className="heart"></div>
+        </div>
+      </div>
       <div className="youtube-imitate">
         {showVideo ? (
           <iframe
             className="video"
             src="https://www.youtube.com/embed/hfjHJneVonE"
+            title="Verka Serduchka - Dancing Lasha Tumbai"
+            loading="lazy"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
           ></iframe>
         ) : (
           <button onClick={() => setShowVideo(true)} className="play"></button>
---
+++ src/js/heartRain.js
@@ -0,0 +1,24 @@
+export default function heartRain() {
+  const raining = () => {
+    let heart = document.querySelector(".heart");
+    let e = document.createElement("div");
+    e.classList.add("drop");
+
+    if (heart) {
+      heart.appendChild(e);
+      let left = Math.floor(Math.random() * 30);
+      let duration = Math.random() * 2;
+
+      e.style.left = left + "rem";
+      e.style.animationDuration = 1 + duration + "s";
+
+      setTimeout(function () {
+        heart.removeChild(e);
+      }, 5000);
+    }
+  };
+
+  setInterval(function () {
+    raining();
+  }, 50);
+}
